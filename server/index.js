0f4f572f0a4f16e059d271ea129c401d1ba376d1
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-03-02 19:02:03 +0100

    added multi-room function

--- server/index.js
+++ server/index.js
@@ -1,20 +1,33 @@
 let WSServer = require('ws').Server,
-    wss = new WSServer({ port: 8080, path: '/room1' }),
-    ws_s = [],
-    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    express = require('express'),
+    app = express(),
+    rooms = {};
 
-wss.on('connection', function (ws) {
-  ws_s.push(ws);
-  console.log('player connected');
+function initRoom(room) {
+  room[0].on('connection', function (ws) {
+    room[2].push(ws);
+    console.log('player connected');
 
-  ws.on('message', function (play) {
-    console.log(play.toString());
-    let game = JSON.parse(play.toString());
-    gameBoard[game['pos']] = game['character'];
-    for (let i = 0; i < ws_s.length; i++) ws_s[i].send(JSON.stringify(gameBoard));
+    ws.on('message', function (play) {
+      console.log(play.toString());
+      let game = JSON.parse(play.toString());
+      room[1][game['pos']] = game['character'];
+      for (let i = 0; i < room[2].length; i++) room[2][i].send(JSON.stringify(room[1]));
+    });
+
+    ws.on('closed', function () {
+      console.log('Player disconnected');
+    })
   });
+}
 
-  ws.on('closed', function () {
-    console.log('Player disconnected');
-  })
+app.post('/createroom', function (req, res) {
+  let token = Math.floor(Math.random() * 1000).toString() + Math.floor(Date.now()).toString();
+  rooms[token] = [new WSServer({ port: 8080, path: '/' + token }), ['', '', '', '', '', '', '', '', ''], []];
+  initRoom(rooms[token]);
+  res.send(token);
 });
+
+app.listen(8080, function() {
+  console.log('Listening on port 8080');
+})

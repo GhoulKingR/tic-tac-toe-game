91109759241fe88b662c5a8018747f8a39c370dd
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-03-02 18:43:09 +0100

    connected separate players together

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 let WSServer = require('ws').Server,
-    wss = new WSServer({ port: 8080, path: '/room1' });
+    wss = new WSServer({ port: 8080, path: '/room1' }),
+    ws_s = [],
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
 
 wss.on('connection', function (ws) {
-  let gameBoard = ['', '', '', '', '', '', '', '', ''];
+  ws_s.push(ws);
   console.log('player connected');
 
   ws.on('message', function (play) {
     console.log(play.toString());
     let game = JSON.parse(play.toString());
     gameBoard[game['pos']] = game['character'];
-    ws.send(JSON.stringify(gameBoard));
+    for (let i = 0; i < ws_s.length; i++) ws_s[i].send(JSON.stringify(gameBoard));
   });
 
   ws.on('closed', function () {

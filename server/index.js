a97b086916623e7657904fc06d2dc3f977b51171
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-03-03 15:18:58 +0100

    added turn features and restricted players to 2

--- server/index.js
+++ server/index.js
@@ -7,22 +7,40 @@ let WSServer = require('ws').Server,
 
 wss.on('connection', function (ws) {
   console.log('player connected');
-
+  
   ws.on('message', function (play) {
     let game = JSON.parse(play.toString()),
-        {token, request, pos, character} = game;
+    {token, request, pos} = game,
+    room = rooms[token];
 
     if (request === 'add') {
       console.log('player added');
-      rooms[token][1].push(ws);
-    } else if (request === 'play') {
-      rooms[token][0][pos] = rooms[token][2];
-      rooms[token][2] = rooms[token][2] == 'x' ? 'o' : 'x';
-
-      for (let i = 0; i < rooms[token][1].length; i++) {
-        rooms[token][1][i].send(
+      
+      if (room['avail_char'].length > 0) {
+        room['ws_s'].push(ws);
+        ws.send(
           JSON.stringify({
-            board: rooms[token][0],
+            type: 'added',
+            character: room['avail_char'].pop(),
+            currentTurn: 'x',
+          })
+        )
+      } else {
+        ws.send(JSON.stringify({
+          type: 'error',
+          message: 'The game is full',
+        }));
+      }
+    } else if (request === 'play') {
+      room['board'][pos] = room['turn'];
+      room['turn'] = room['turn'] === 'x' ? 'o' : 'x';
+
+      for (let i = 0; i < room['ws_s'].length; i++) {
+        room['ws_s'][i].send(
+          JSON.stringify({
+            type: 'play',
+            board: room['board'],
+            currentTurn: room['turn']
           })
         )
       }
@@ -34,7 +52,11 @@ app.use(cors());
 
 app.post('/createroom', function (req, res) {
   let token = Math.floor(Math.random() * 1000).toString() + Math.floor(Date.now()).toString();
-  rooms[token] = [['', '', '', '', '', '', '', '', ''], [], 'x'];
+  rooms[token] = {
+    board: ['', '', '', '', '', '', '', '', ''], 
+    ws_s: [], 
+    turn: 'x',
+    avail_char: ['o', 'x']};
   res.send(token);
 });
 

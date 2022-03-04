a95071e18f330677d877ab474f7d683da81b81b5
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-03-04 16:30:26 +0100

    major refactorings

--- server/index.js
+++ server/index.js
@@ -14,50 +14,56 @@ wss.on('connection', function (ws) {
         room = rooms[token];
         
     if (request === 'reset') resetGame(room['ws_s'], game['from']);
-    else if (request === 'add') {
-      console.log('player added');
-      
-      if (room['avail_char'].length > 0) {
-        room['ws_s'].push(ws);
-        ws.send(
-          JSON.stringify({
-            type: 'added',
-            character: room['avail_char'].pop(),
-            currentTurn: 'x',
-          })
-        )
-      } else {
-        ws.send(JSON.stringify({
-          type: 'error',
-          message: 'The game is full',
-        }));
-      }
-    } else if (request === 'play') {
-      room['board'][pos] = room['turn'];
-      room['turn'] = invert(room['turn']);
-
-      for (let i = 0; i < room['ws_s'].length; i++) {
-        room['ws_s'][i].send(
-          JSON.stringify(
-            !isWinner(room['board'])
-              ? {
-                  type: 'play',
-                  board: room['board'],
-                  currentTurn: room['turn']
-                }
-              : {
-                  type: 'winner',
-                  board: room['board'],
-                  winner: invert(room['turn'])
-                }
-            )
-        )
-      }
-    } else {
+    else if (request === 'add') addPlayer(room, ws);
+    else if (request === 'play') playGame(room, pos);
+    else {
       throw new Error("The request: " + request + " is not implemented");
     }
   });
 });
+
+function playGame(room, pos) {
+  room['board'][pos] = room['turn'];
+  room['turn'] = invert(room['turn']);
+  
+  let message = JSON.stringify(
+    !isWinner(room['board'])
+      ? {
+          type: 'play',
+          board: room['board'],
+          currentTurn: room['turn']
+        }
+      : {
+          type: 'winner',
+          board: room['board'],
+          winner: invert(room['turn'])
+        }
+  );
+
+  for (let i = 0; i < room['ws_s'].length; i++) {
+    room['ws_s'][i].send(message);
+  }
+}
+
+function addPlayer(room, ws) {
+  console.log('player added');
+  let message = "";
+
+  if (room['avail_char'].length > 0) {
+    room['ws_s'].push(ws);
+    message = JSON.stringify({
+      type: 'added',
+      character: room['avail_char'].pop(),
+      currentTurn: 'x',
+    })  
+  } else {
+    message = JSON.stringify({
+      type: 'error',
+      message: 'The game is full',
+    });
+  }
+  ws.send(message)
+}
 
 function resetGame(ws_s, from) {
   let message = JSON.stringify({
@@ -76,25 +82,21 @@ function invert(turn) {
 
 function isWinner(board) {
   // horizontals
-  for (let i = 0; i < 3; i++) {
-    if (board[i + 0] === board[i + 1] && board[i + 1] === board[i + 2] && board[i] !== '') {
+  for (let i = 0; i < 3; i++)
+    if (board[i + 0] === board[i + 1] && board[i + 1] === board[i + 2] && board[i] !== '') 
       return true;
-    }
-  }
+
   // verticals 
-  for (let i = 0; i < 3; i++) {
-    if (board[i] === board[i + 3] && board[i + 3] === board[i + 6] && board[i] !== '') {
+  for (let i = 0; i < 3; i++)
+    if (board[i] === board[i + 3] && board[i + 3] === board[i + 6] && board[i] !== '')
       return true;
-    }
-  }
+    
   // diagonals
   if (
     ((board[0] === board[4] && board[4] === board[8])
     || (board[2] === board[4] && board[4] === board[6]))
     && board[4] !== ''
-  ) {
-    return true;
-  }
+  ) return true;
 
 
   return false;

e6ca5ab4bac881400efc403e002dfc63db4b9761
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-08-12 14:18:44 +0100

    created outline

--- components/ConnectWallet.js
+++ components/ConnectWallet.js
@@ -1,4 +1,5 @@
 import { loadStdlib, ALGO_MyAlgoConnect as MyAlgoConnect } from '@reach-sh/stdlib';
+import React from 'react';
 
 const reach = loadStdlib(process.env);
 reach.setWalletFallback(reach.walletFallback({
@@ -18,3 +19,17 @@ function ConnectWallet({ setAcc, openCreateJoin }) {
 }
 
 export default ConnectWallet;
+
+/**
+ * OUTLINE
+ * 
+ * What is this project?
+ * - Describes this project
+ * 
+ * What do you need to run this project?
+ * - Lists the tools you need to run the project without problems
+ * 
+ * What next.
+ * - Describes the next action to play the game
+ * 
+ */
